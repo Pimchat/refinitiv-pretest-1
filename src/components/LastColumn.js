a6a2eda2babf8d67a5da8fd687b111c69fb40075
PIMCHAT PANICHSUKKO <pim.pimchat@gmail.com>
2021-07-13 18:20:33 +0700

    solve merged conflict

--- src/components/LastColumn.js
+++ src/components/LastColumn.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./LastColumn.css";
 
@@ -11,13 +10,3 @@ function LastCol({ result }) {
 }
 
 export default LastCol;
-=======
-import React from "react";
-import "./LastColumn.css";
-
-function LastCol({ result }) {
-  return <div id="last-col">result: {result}</div>;
-}
-
-export default LastCol;
->>>>>>> 4de6b1f4cc2757b5b90f0a0d2be3a05123ad83d7
